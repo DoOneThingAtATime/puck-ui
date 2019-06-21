e881a6e456a9338ef8631694c7e6c5414b181559
guakun <guakun@outlook.com>
2019-06-21 14:00:38 +0800

    初始化项目

---
+++ lib/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import Icon from './icon';
+
+ReactDOM.render(<div>
+    <Icon name="baidu" />
+    <Icon name="alipay" />
+    <Icon name="wechat" />
+  </div>,
+  document.querySelector('#root'))
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    puck: './lib/index.tsx'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+      }
+    ]
+  },
+}
