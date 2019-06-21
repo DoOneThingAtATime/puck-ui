8ce3219a46c495b275049195e47c64e659cb552c
guakun <guakun@outlook.com>
2019-06-21 14:47:09 +0800

    添加 icon 的 onClick 事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -4,11 +4,13 @@ import './icon.scss'
 
 interface IconProps {
   name: string;
+  onClick: () => void;
+  // onClick: React.MouseEventHandler<SVGElement>
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <svg className="puck-icon">
+    <svg className="puck-icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`} />
     </svg>
   )
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import Icon from './icon';
 
+const fn = () => {
+  console.log('fn')
+}
+
 ReactDOM.render(<div>
-    <Icon name="baidu" />
-    <Icon name="alipay" />
-    <Icon name="wechat" />
+    <Icon name="wechat" onClick={fn}/>
   </div>,
   document.querySelector('#root'))
