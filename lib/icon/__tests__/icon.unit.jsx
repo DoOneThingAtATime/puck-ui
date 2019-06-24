cbf3409805053c544e4e2459da08970283e3e1f0
guakun <guakun@outlook.com>
2019-06-24 13:58:51 +0800

    icon 测试

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,10 +1,18 @@
-import * as renderer from 'react-test-renderer'
-import React from 'react'
-import Icon from '../icon'
+import * as renderer from 'react-test-renderer';
+import React from 'react';
+import Icon from '../icon';
+import { mount } from 'enzyme';
 
 describe('icon', () => {
-  it('是个 div', () => {
-    const json = renderer.create(<Icon name="alipay" />).toJSON()
-    expect(json).toMatchSnapshot()
-  })
-})
+  it('render succcessfully', () => {
+    const json = renderer.create(<Icon name="alipay" />).toJSON();
+    expect(json).toMatchSnapshot();
+  });
+
+  it('onClick', () => {
+    const fn = jest.fn()
+    const component = mount(<Icon name="alipay" onClick={fn}/>)
+    component.find('svg').simulate('click')
+    expect(fn).toBeCalled()
+  });
+});
