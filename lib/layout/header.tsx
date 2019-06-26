5ba04b081083e050713f29e7301f365f791fbbf5
guakun <guakun@outlook.com>
2019-06-26 11:20:12 +0800

    完成 layout 等五个组件

---
+++ lib/layout/header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { scopedClassMaker } from '../classes';
+
+const sc = scopedClassMaker('puck-layout');
+interface Props extends React.HTMLAttributes<HTMLElement> {}
+
+const Header: React.FunctionComponent<Props> = props => {
+  const { className, ...rest } = props;
+  return (
+    <div className={sc('header', { extra: className })} {...rest}>
+      header
+    </div>
+  );
+};
+
+export default Header;
