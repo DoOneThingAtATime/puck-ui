44bb81228fc2a5d8aad68f1ef5599e111f26e4c6
guakun <guakun@outlook.com>
2019-06-26 14:31:21 +0800

    重构 classes

--- lib/helper/classes.tsx
+++ lib/helper/classes.tsx
@@ -13,32 +13,22 @@ interface ClassToggles {
 }
 
 function scopedClassMaker(prefix: string) {
-  return function(name?: string | ClassToggles, options?: Options) {
-    // name = {hasAisde: true, active: false, x: true, y: false}
-    let name2;
-    let result;
-    if (typeof name === 'string' || name === undefined) {
-      name2 = name;
-      result = [prefix, name2].filter(Boolean).join('-');
-    } else {
-      // ['hasAisde', 'x']
-      name2 = Object.entries(name)
-        .filter(kv => kv[1])
-        .map(kv => kv[0]);
-      console.log('name2')
-      console.log(name2)
-      result = name2.map(n => {
-        return [prefix, n].filter(Boolean).join('-');
-      }).join(' ')
-      console.log('result')
-      console.log(result)
-      // ['puck-layout-hasAside', 'puck-layout-x']
-    }
+  return function(name: string | ClassToggles, options?: Options) {
+    const namesObject =
+      typeof name === 'string' || name === undefined ? { [name]: name } : name;
+
+    const scoped = Object.entries(namesObject)
+      .filter(kv => kv[1] !== false)
+      .map(kv => kv[0])
+      .map(name => {
+        return [prefix, name].filter(Boolean).join('-');
+      })
+      .join(' ');
 
     if (options && options.extra) {
-      return [result, options.extra].filter(Boolean).join(' ');
+      return [scoped, options.extra].filter(Boolean).join(' ');
     } else {
-      return result;
+      return scoped;
     }
   };
 }
